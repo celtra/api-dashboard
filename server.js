3f9d3955f960117a2baf81bc0e4d95d3b71cd049
atanko123 <anze.8tanko@gmail.com>
2021-04-20 14:18:00 +0200

    Server side update

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 // server.js
-var PRODUCTION = true;
 
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-
-if (PRODUCTION) {
-    app.use(serveStatic(path.join(path.resolve(), 'dist')));
-}
-else {
-    app.use(serveStatic(__dirname + "/dist"));
-}
-
+app.use(serveStatic(path.join(path.resolve(), 'dist')));
 var port = process.env.PORT || 3000;
+
 app.listen(port, () => {
     console.log("Server running at port", port);
 });
