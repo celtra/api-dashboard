64969247213e485170bd8b681eaa5aa1e65f7eab
atanko123 <anze.8tanko@gmail.com>
2021-04-21 13:38:58 +0200

    Basic css added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h2>Ad products</h2>
     <Creatives />
   </div>
 </template>
@@ -23,12 +24,24 @@ export default {
 </script>
 
 <style>
+  body, html {
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    min-height: 100%;
+     background-color: #000014;
+  }
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    margin: 0;
+  }
+
+  h2 {
+    color: white;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
   }
 </style>
